6eba34c16930049f5f48b2b3d7b04578a39c6a17
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-05-21 10:11:48 +0100

    Handle no questionnaire found

--- extract_call_history.py
+++ extract_call_history.py
@@ -5,7 +5,7 @@ from models.call_history import CallHistory
 def get_questionnaire_name_from_id(questionnaire_id, questionnaire_list):
     return next(
         (item for item in questionnaire_list if item.get("id") == questionnaire_id),
-        None,
+        {"name": ""},
     ).get("name")
 
 
@@ -19,7 +19,8 @@ def load_cati_dial_history(config, questionnaire_list):
         questionnaire_name = get_questionnaire_name_from_id(
             call_history.questionnaire_id, questionnaire_list
         )
-        call_history.generate_questionnaire_details(questionnaire_name)
+        if questionnaire_name is not "":
+            call_history.generate_questionnaire_details(questionnaire_name)
         call_history_list.append(call_history)
 
     print(
