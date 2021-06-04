f692d5c372a9a4332ac0ca2372ab2d82776e48c1
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2021-06-04 12:05:35 +0100

    test_split_into_batches

--- tests/data_sources/test_datastore.py
+++ tests/data_sources/test_datastore.py
@@ -49,7 +49,7 @@ def test_date_string_to_datetime_returns_valid_date_with_end_time(date_string, e
 def test_date_string_to_datetime_returns_none_if_date_is_invalid(date_string):
     date_time = date_string_to_datetime(date_string)
 
-    assert date_time == None
+    assert date_time is None
 
 
 @pytest.mark.parametrize(
