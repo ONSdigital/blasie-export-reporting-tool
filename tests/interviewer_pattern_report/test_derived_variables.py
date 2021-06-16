c5b9288e4e3d520d4ccb559894ea3008d417d017
elthorne <46898565+elthorne@users.noreply.github.com>
2021-06-16 11:44:51 +0100

    Refactored get_hours() test

--- tests/interviewer_pattern_report/test_derived_variables.py
+++ tests/interviewer_pattern_report/test_derived_variables.py
@@ -7,27 +7,8 @@ from interviewer_pattern_report.derived_variables import get_hours_worked, get_t
     get_percentage_of_time_on_calls
 
 
-def test_get_hours_worked():
-    data = [
-        {
-            'call_start_time': DatetimeWithNanoseconds(2021, 1, 1, 10, 00, 00, tzinfo=datetime.timezone.utc),
-            'call_end_time': DatetimeWithNanoseconds(2021, 1, 1, 10, 30, 00, tzinfo=datetime.timezone.utc)
-        },
-        {
-            'call_start_time': DatetimeWithNanoseconds(2021, 1, 11, 00, 00, 00, tzinfo=datetime.timezone.utc),
-            'call_end_time': DatetimeWithNanoseconds(2021, 1, 1, 11, 30, 00, tzinfo=datetime.timezone.utc)
-        },
-        {
-            'call_start_time': DatetimeWithNanoseconds(2021, 1, 2, 10, 00, 00, tzinfo=datetime.timezone.utc),
-            'call_end_time': DatetimeWithNanoseconds(2021, 1, 2, 11, 00, 00, tzinfo=datetime.timezone.utc)
-        },
-        {
-            'call_start_time': DatetimeWithNanoseconds(2021, 1, 5, 14, 00, 00, tzinfo=datetime.timezone.utc),
-            'call_end_time': DatetimeWithNanoseconds(2021, 1, 5, 16, 00, 00, tzinfo=datetime.timezone.utc)
-        }
-    ]
-    df_data = pd.DataFrame(data)
-    assert get_hours_worked(df_data) == "4:30:00"
+def test_get_hours_worked(mock_data):
+    assert get_hours_worked(mock_data) == "15:00:00"
 
 
 def test_get_total_call_seconds():
