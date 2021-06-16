70e2a3c03d2d07d1e39e1dab85f6825bd4e536ca
elthorne <46898565+elthorne@users.noreply.github.com>
2021-06-16 11:44:28 +0100

    Refactored get report to return dictionary

--- interviewer_pattern_report/report.py
+++ interviewer_pattern_report/report.py
@@ -9,9 +9,25 @@ def get_call_pattern_records_by_interviewer_and_date_range(interviewer_name, sta
     error, call_history = get_call_history_records_by_interviewer_and_date_range(
         interviewer_name, start_date_string, end_date_string
     )
-    df_call_history = pd.DataFrame(call_history)
-    hours_worked = get_hours_worked(df_call_history)
-    total_call_seconds = get_total_call_seconds(df_call_history)
-    percentage_of_time_on_calls = get_percentage_of_time_on_calls(hours_worked, total_call_seconds)
-    df_call_pattern = pd.DataFrame([hours_worked]).append([total_call_seconds]).append([percentage_of_time_on_calls])
-    return None, df_call_pattern
+
+    if error:
+        print(f"get_call_pattern_records_by_interviewer_and_date_range failed because {error}")
+        return
+
+    interviewer_pattern_dict = {
+        'Hours worked': get_hours_worked(call_history),
+        # 'Call time': get_call_time(call_history_dataframe),
+        # '% Hours on calls': f"{get_percentage_of_time_on_calls(get_hours_worked(call_history_dataframe), get_call_time(call_history_dataframe))}%",
+        # 'Ave calls per working hour': get_average_calls_per_hour,
+    }
+
+    # TODO: check some errorrrrs before returning!
+    return None, interviewer_pattern_dict
+
+    # df_call_history = pd.DataFrame(call_history)
+    # hours_worked = get_hours_worked(df_call_history)
+    # total_call_seconds = get_total_call_seconds(df_call_history)
+    # percentage_of_time_on_calls = get_percentage_of_time_on_calls(hours_worked, total_call_seconds)
+    # df_call_pattern = pd.DataFrame([hours_worked]).append([total_call_seconds]).append([percentage_of_time_on_calls])
+    # return None, df_call_pattern
+
