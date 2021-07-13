422b89e0bead6b97e36e59f9d1b9106c23d0743e
Riceyo <richmond@ricey.org>
2021-07-13 13:44:17 +0100

    trying to fix tmp folder

--- definitions.py
+++ definitions.py
@@ -1,6 +1,6 @@
 import os
 
-if os.getenv("GAE_ENV", "").startswith("standard"):
+if os.getenv("FUNCTION_NAME", "") == "bert-deliver-mi-hub-reports":
     ROOT_DIR = "/"
 else:
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
