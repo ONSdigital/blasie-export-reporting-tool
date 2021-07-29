709c968a0c9e61503575069b3aa241d443383d91
Riceyo <richmond@ricey.org>
2021-07-29 17:19:05 +0100

    oops

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -129,6 +129,6 @@ def test_call_pattern_report(mock_get_call_pattern_records_by_interviewer_and_da
 @patch("app.app.get_appointment_resource_planning_by_date")
 def test_appointment_resource_planning(mock_get_appointment_resource_planning_by_date, client):
     mock_get_appointment_resource_planning_by_date.return_value = None, []
-    response = client.get("/api/appointment-resource-planning/2021-01-01")
+    response = client.get("/api/reports/appointment-resource-planning/2021-01-01")
     assert response.status_code == 200
     assert response.get_data() is not None
