25315483d41e89cf04ce60b377a4db8e9a132634
elthorne <46898565+elthorne@users.noreply.github.com>
2021-08-06 10:35:14 +0100

    Refactored: re-jigged order of tests because I'm weird like that

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -30,7 +30,7 @@ def test_call_history_report_with_survey_tla(mock_get_call_history_records_by_in
 @patch("app.app.get_call_history_records_by_interviewer_and_date_range")
 def test_call_history_report(mock_get_call_history_records_by_interviewer_and_date_range, client):
     mock_get_call_history_records_by_interviewer_and_date_range.return_value = []
-    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01&end-date=2021-01-01")
+    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01&end-date=2021-01-01&survey-tla=opn")
     assert response.status_code == 200
     assert json.loads(response.get_data(as_text=True)) == []
 
@@ -44,15 +44,7 @@ def test_call_history_report_returns_error(mock_get_call_history_records_by_inte
 
 
 @patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
-def test_call_pattern_report_returns_error(mock_get_call_pattern_records_by_interviewer_and_date_range, client):
-    mock_get_call_pattern_records_by_interviewer_and_date_range.side_effect = BertException("Invalid date range parameters provided", 400)
-    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid date range parameters provided"}'
-
-
-@patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
-def test_call_pattern_report_with_survey_tla(mock_get_call_pattern_records_by_interviewer_and_date_range, client,
+def test_call_pattern_report(mock_get_call_pattern_records_by_interviewer_and_date_range, client,
                              interviewer_call_pattern_report):
     mock_get_call_pattern_records_by_interviewer_and_date_range.return_value = interviewer_call_pattern_report
     response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01&survey-tla=opn")
@@ -61,12 +53,11 @@ def test_call_pattern_report_with_survey_tla(mock_get_call_pattern_records_by_in
 
 
 @patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
-def test_call_pattern_report(mock_get_call_pattern_records_by_interviewer_and_date_range, client,
-                             interviewer_call_pattern_report):
-    mock_get_call_pattern_records_by_interviewer_and_date_range.return_value = interviewer_call_pattern_report
+def test_call_pattern_report_returns_error(mock_get_call_pattern_records_by_interviewer_and_date_range, client):
+    mock_get_call_pattern_records_by_interviewer_and_date_range.side_effect = BertException("Invalid date range parameters provided", 400)
     response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01")
-    assert response.status_code == 200
-    assert response.get_data(as_text=True) == interviewer_call_pattern_report.json()
+    assert response.status_code == 400
+    assert response.get_data(as_text=True) == '{"error": "Invalid date range parameters provided"}'
 
 
 @patch("app.app.get_appointment_resource_planning_by_date")
