43832a2e89df5818be5af40ecf8e77e52b2cb1df
elthorne <46898565+elthorne@users.noreply.github.com>
2021-08-06 09:10:44 +0100

    Refactored: abstracted date_handler from date_functions.py to request_handlers.py

--- functions/date_functions.py
+++ functions/date_functions.py
@@ -1,6 +1,5 @@
 import re
 from datetime import datetime
-from models.error_capture import BertException
 
 
 def validate_date(date_text):
@@ -22,16 +21,3 @@ def parse_date_string_to_datetime(date_string, end_of_day=False):
             int(date_split[0]), int(date_split[1]), int(date_split[2]), 23, 59, 59
         )
     return datetime(int(date_split[0]), int(date_split[1]), int(date_split[2]))
-
-
-def date_handler(request):
-    start_date = request.args.get("start-date", None)
-    end_date = request.args.get("end-date", None)
-
-    if start_date is None or end_date is None:
-        raise(BertException("Invalid request, missing required date parameters", 400))
-
-    if not validate_date(start_date) or not validate_date(end_date):
-        raise(BertException("Invalid request, date is not valid", 400))
-
-    return start_date, end_date
