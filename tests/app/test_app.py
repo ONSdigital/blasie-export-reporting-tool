ccaaee8147dc5509dd38c2a671c513a61b6f9937
elthorne <46898565+elthorne@users.noreply.github.com>
2021-08-06 09:15:18 +0100

    Refactored: removed excessive tests as date and survey tlas are now unit tested in test_request_handlers.py

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -19,40 +19,12 @@ def test_call_history_report_status(mock_get_call_history_report_status, client)
     assert response.get_data() is not None
 
 
-def test_call_history_report_returns_error_when_start_date_parameter_is_not_provided(client):
-    response = client.get("/api/reports/call-history/matpal?end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, missing required date parameters"}'
-
-
-def test_call_history_report_returns_error_when_start_date_value_is_not_provided(client):
-    response = client.get("/api/reports/call-history/matpal?start-date=&end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_history_report_returns_error_when_start_date_value_is_not_a_valid_date(client):
-    response = client.get("/api/reports/call-history/matpal?start-date=blah&end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_history_report_returns_error_when_end_date_parameter_is_not_provided(client):
-    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, missing required date parameters"}'
-
-
-def test_call_history_report_returns_error_when_end_date_value_is_not_provided(client):
-    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01&end-date=")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_history_report_returns_error_when_end_date_value_is_not_a_valid_date(client):
-    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01&end-date=blah")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
+@patch("app.app.get_call_history_records_by_interviewer_and_date_range")
+def test_call_history_report_with_survey_tla(mock_get_call_history_records_by_interviewer_and_date_range, client):
+    mock_get_call_history_records_by_interviewer_and_date_range.return_value = []
+    response = client.get("/api/reports/call-history/matpal?start-date=2021-01-01&end-date=2021-01-01&survey-tla=opn")
+    assert response.status_code == 200
+    assert json.loads(response.get_data(as_text=True)) == []
 
 
 @patch("app.app.get_call_history_records_by_interviewer_and_date_range")
@@ -71,50 +43,6 @@ def test_call_history_report_returns_error(mock_get_call_history_records_by_inte
     assert response.get_data(as_text=True) == '{"error": "Invalid date range parameters provided"}'
 
 
-def test_call_pattern_report_returns_error_when_start_date_parameter_is_not_provided(client):
-    response = client.get("/api/reports/call-pattern/matpal?end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, missing required date parameters"}'
-
-
-def test_call_pattern_report_returns_error_when_start_date_value_is_not_provided(client):
-    response = client.get("/api/reports/call-pattern/matpal?start-date=&end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_pattern_report_returns_error_when_start_date_value_is_not_a_valid_date(client):
-    response = client.get("/api/reports/call-pattern/matpal?start-date=blah&end-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_pattern_report_returns_error_when_end_date_parameter_is_not_provided(client):
-    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, missing required date parameters"}'
-
-
-def test_call_pattern_report_returns_error_when_end_date_value_is_not_provided(client):
-    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-def test_call_pattern_report_returns_error_when_end_date_value_is_not_a_valid_date(client):
-    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=blah")
-    assert response.status_code == 400
-    assert response.get_data(as_text=True) == '{"error": "Invalid request, date is not valid"}'
-
-
-@patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
-def test_call_pattern_report(mock_get_call_pattern_records_by_interviewer_and_date_range, client, mock_report):
-    mock_get_call_pattern_records_by_interviewer_and_date_range.return_value = (None, 200), mock_report
-    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01")
-    assert response.status_code == 200
-    assert response.get_data(as_text=True) == mock_report.json()
-
-
 @patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
 def test_call_pattern_report_returns_error(mock_get_call_pattern_records_by_interviewer_and_date_range, client):
     mock_get_call_pattern_records_by_interviewer_and_date_range.side_effect = BertException("Invalid date range parameters provided", 400)
@@ -124,9 +52,26 @@ def test_call_pattern_report_returns_error(mock_get_call_pattern_records_by_inte
 
 
 @patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
+def test_call_pattern_report_with_survey_tla(mock_get_call_pattern_records_by_interviewer_and_date_range, client,
+                             interviewer_call_pattern_report):
+    mock_get_call_pattern_records_by_interviewer_and_date_range.return_value = interviewer_call_pattern_report
+    response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01&survey-tla=opn")
+    assert response.status_code == 200
+    assert response.get_data(as_text=True) == interviewer_call_pattern_report.json()
+
+
+@patch("app.app.get_call_pattern_records_by_interviewer_and_date_range")
 def test_call_pattern_report(mock_get_call_pattern_records_by_interviewer_and_date_range, client,
                              interviewer_call_pattern_report):
     mock_get_call_pattern_records_by_interviewer_and_date_range.return_value = interviewer_call_pattern_report
     response = client.get("/api/reports/call-pattern/matpal?start-date=2021-01-01&end-date=2021-01-01")
     assert response.status_code == 200
     assert response.get_data(as_text=True) == interviewer_call_pattern_report.json()
+
+
+@patch("app.app.get_appointment_resource_planning_by_date")
+def test_appointment_resource_planning(mock_get_appointment_resource_planning_by_date, client):
+    mock_get_appointment_resource_planning_by_date.return_value = None, []
+    response = client.get("/api/reports/appointment-resource-planning/2021-01-01")
+    assert response.status_code == 200
+    assert response.get_data() is not None
