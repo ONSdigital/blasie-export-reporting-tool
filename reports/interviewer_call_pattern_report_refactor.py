4477446be6a7da8c6cc6f31691974ba5aba66461
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2021-09-13 16:39:17 +0100

    LU-8867_refactor.
    Added more unit tests and functionality for the call time and average calls per hour report attributes.

--- reports/interviewer_call_pattern_report_refactor.py
+++ reports/interviewer_call_pattern_report_refactor.py
@@ -1,6 +1,7 @@
 from google.cloud import datastore
 import datetime
 import pandas as pd
+import numpy as np
 
 
 def get_call_pattern_report():
@@ -8,30 +9,89 @@ def get_call_pattern_report():
     if records.empty:
         return {}
 
-    hours_worked = calculate_hours_worked(records)
-    discounted_invalid_cases = calculate_number_of_invalid_cases
+    records = replace_empty_strings_with_nans(records)
 
-    return {"hours_worked": hours_worked}
+    hours_worked_in_seconds = calculate_hours_worked_in_seconds(records)
+    number_of_invalid_records = calculate_number_of_invalid_records(records)
+    reason_for_invalid_fields = provide_reason_for_invalid_records(records)
+    call_time = calculate_call_time(records)
+    hours_on_call_percentage = calculate_hours_on_call_percentage(records)
+    average_calls_per_hour = calculate_average_calls_per_hour(records)
+
+    return {
+        "hours_worked": str(datetime.timedelta(seconds=hours_worked_in_seconds)),
+        "discounted_invalid_cases": format_fraction_and_percentage_as_string(number_of_invalid_records, len(records)),
+        "invalid_fields": reason_for_invalid_fields,
+        "call_time": str(datetime.timedelta(seconds=call_time)),
+        "hours_on_call_percentage": f"{hours_on_call_percentage}%",
+        "average_calls_per_hour": average_calls_per_hour
+    }
+
 
 def get_call_history_records():
     client = datastore.Client()
     query = client.query(kind="CallHistory")
     return pd.DataFrame(list(query.fetch()))
 
-def calculate_hours_worked(records):
-    records = records[records.call_end_time != '']
+
+def replace_empty_strings_with_nans(records: pd.DataFrame) -> pd.DataFrame:
+    """Fill all cells containing empty strings or None values with NAN values.
+
+    NAN is easier to filter when selecting the valid and invalid records.
+    """
+    return records.replace("", np.nan).fillna(value=np.nan)
+
+
+def format_fraction_and_percentage_as_string(numerator: int, denominator: int) -> str:
+    """Format the result as a string displaying both the fraction and percentage values.
+
+    Example: 2/4, 50%
+    """
+    percentage = format(numerator / denominator * 100, '.2f')
+    return f"{numerator}/{denominator}, {percentage}%"
+
+
+def calculate_hours_worked_in_seconds(records: pd.DataFrame) -> int:
     records = records.dropna(subset=["call_end_time"])
 
-    # group by date
+    # Group records by date.
     daily_call_history_by_date = records.groupby(
         [records['call_start_time'].dt.date]).agg({'call_start_time': min, 'call_end_time': max})
 
-    # subtract first call time from last call time
+    # Subtract the first call time from the last call time.
     daily_call_history_by_date['hours_worked'] = daily_call_history_by_date['call_end_time'] - \
                                                  daily_call_history_by_date['call_start_time']
 
-    # sum total hours
-    total_hours = daily_call_history_by_date['hours_worked'].sum()
+    # Sum total hours.
+    return daily_call_history_by_date['hours_worked'].sum().total_seconds()
 
-    # return sum total in fancy format
-    return str(datetime.timedelta(seconds=total_hours.total_seconds()))
+
+def calculate_number_of_invalid_records(records: pd.DataFrame) -> str:
+    return len(records[records["call_end_time"].isna()])
+
+
+def provide_reason_for_invalid_records(records: pd.DataFrame) -> str:
+    """."""
+    invalid_records = records[records["call_end_time"].isna()]
+    status = invalid_records['status'].unique()
+
+    if 'Timed out' in status:
+        return "'status' column had timed out call status"
+    return ""
+
+def calculate_call_time(records: pd.DataFrame) -> str:
+    return round(records['dial_secs'].sum())
+
+
+def calculate_hours_on_call_percentage(records: pd.DataFrame) -> float:
+    call_time_in_seconds = round(records['dial_secs'].sum())
+
+    hours_worked_in_seconds = calculate_hours_worked_in_seconds(records)
+
+    return round(call_time_in_seconds / hours_worked_in_seconds * 100, 2)
+
+
+def calculate_average_calls_per_hour(records) -> float:
+    hours_worked = calculate_hours_worked_in_seconds(records) / 3600
+    number_of_valid_records = records["call_end_time"].count()
+    return number_of_valid_records / float(hours_worked)
--- tests/reports/test_interviewer_call_pattern_report_refactor.py
+++ tests/reports/test_interviewer_call_pattern_report_refactor.py
@@ -12,6 +12,7 @@ def test_get_call_pattern_report_returns_an_empty_dict_if_no_records_were_found(
 
     assert get_call_pattern_report() == {}
 
+
 def test_get_call_pattern_report_returns_hours_worked_when_a_record_is_found(mocker):
     datastore_records = [
         {
@@ -44,6 +45,7 @@ def test_get_call_pattern_report_returns_hours_worked_when_a_record_is_found(moc
         return_value=pd.DataFrame(datastore_records))
     result = get_call_pattern_report()
     assert result["hours_worked"] == "6:00:00"
+
 
 def test_get_call_pattern_report_returns_hours_worked_when_multiple_records_from_a_single_day_are_found(mocker):
     datastore_records = [
@@ -101,6 +103,7 @@ def test_get_call_pattern_report_returns_hours_worked_when_multiple_records_from
 
     result = get_call_pattern_report()
     assert result["hours_worked"] == "8:00:00"
+
 
 def test_get_call_pattern_report_returns_hours_worked_when_multiple_records_from_multiple_days_are_found(mocker):
     datastore_records = [
@@ -181,6 +184,7 @@ def test_get_call_pattern_report_returns_hours_worked_when_multiple_records_from
 
     result = get_call_pattern_report()
     assert result["hours_worked"] == "9:00:00"
+
 
 def test_get_call_pattern_report_ignores_record_when_no_end_call_time_is_found(mocker):
     datastore_records = [
@@ -281,10 +285,11 @@ def test_get_call_pattern_report_ignores_record_when_no_end_call_time_is_found(m
     result = get_call_pattern_report()
     assert result["hours_worked"] == "2:00:00"
 
+
 def test_get_call_pattern_report_returns_number_of_records_where_no_end_call_time_is_found(mocker):
     datastore_records = [
         {
-            "call_number": 2,
+            "call_number": 1,
             "call_result": "AnswerService",
             "call_start_time": DatetimeWithNanoseconds(
                 2021, 8, 7, 8, 00, 00, tzinfo=datetime.timezone.utc
@@ -310,7 +315,7 @@ def test_get_call_pattern_report_returns_number_of_records_where_no_end_call_tim
             "call_start_time": DatetimeWithNanoseconds(
                 2021, 8, 7, 12, 00, 00, tzinfo=datetime.timezone.utc
             ),
-            "call_end_time": pd.NaT,
+            "call_end_time": None,
             "cohort": None,
             "dial_number": 1,
             "dial_secs": 8,
@@ -326,10 +331,10 @@ def test_get_call_pattern_report_returns_number_of_records_where_no_end_call_tim
             "wave": None,
         },
         {
-            "call_number": 1,
+            "call_number": 3,
             "call_result": "AnswerService",
             "call_start_time": DatetimeWithNanoseconds(
-                2021, 8, 7, 15, 00, 00, tzinfo=datetime.timezone.utc
+                2021, 8, 9, 15, 00, 00, tzinfo=datetime.timezone.utc
             ),
             "call_end_time": DatetimeWithNanoseconds(
                 2021, 8, 7, 16, 00, 00, tzinfo=datetime.timezone.utc
@@ -349,7 +354,7 @@ def test_get_call_pattern_report_returns_number_of_records_where_no_end_call_tim
             "wave": None,
         },
         {
-            "call_number": 1,
+            "call_number": 4,
             "call_result": "AnswerService",
             "call_start_time": DatetimeWithNanoseconds(
                 2021, 8, 9, 15, 00, 00, tzinfo=datetime.timezone.utc
@@ -379,3 +384,381 @@ def test_get_call_pattern_report_returns_number_of_records_where_no_end_call_tim
 
     result = get_call_pattern_report()
     assert result["discounted_invalid_cases"] == "2/4, 50.00%"
+
+
+def test_get_call_pattern_report_returns_timed_out_status_when_no_end_call_time_is_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 8, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": None,
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 2,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 12, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 14, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Finished (No contact)",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["invalid_fields"] == "'status' column had timed out call status"
+
+
+def test_get_call_pattern_report_returns_call_time_when_one_record_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 8, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["call_time"] == "0:10:00"
+
+def test_get_call_pattern_report_returns_call_time_when_multiple_records_are_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 8, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 2,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 11, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 12, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["call_time"] == "0:20:00"
+
+def test_get_call_pattern_report_returns_hours_on_call_as_perecntage_of_worked_time_when_one_record_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["hours_on_call_percentage"] == "16.67%"
+
+
+def test_get_call_pattern_report_returns_hours_on_call_as_perecntage_of_worked_time_when_multiple_records_are_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 2,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["hours_on_call_percentage"] == "33.33%"
+
+def test_get_call_pattern_report_returns_average_calls_per_hour(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 600,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Timed out",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["average_calls_per_hour"] == 1.0
+
+
+def test_get_call_pattern_report_returns_average_calls_per_hour_when_multiple_records_are_found(mocker):
+    datastore_records = [
+        {
+            "call_number": 1,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Finished (No contact)",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 2,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Finished (No contact)",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 3,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Finished (No contact)",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+        {
+            "call_number": 4,
+            "call_result": "AnswerService",
+            "call_start_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 9, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "call_end_time": DatetimeWithNanoseconds(
+                2021, 8, 7, 10, 00, 00, tzinfo=datetime.timezone.utc
+            ),
+            "cohort": None,
+            "dial_number": 1,
+            "dial_secs": 8,
+            "interviewer": "el4president",
+            "number_of_interviews": "1",
+            "outcome_code": 310,
+            "questionnaire_id": "remember-24-01-9dc791f0cb07",
+            "questionnaire_name": "OPN2108R",
+            "serial_number": 24012022,
+            "status": "Finished (No contact)",
+            "survey": "OPN",
+            "update_info": None,
+            "wave": None,
+        },
+    ]
+
+    mocker.patch(
+        "reports.interviewer_call_pattern_report_refactor.get_call_history_records",
+        return_value=pd.DataFrame(datastore_records))
+
+    result = get_call_pattern_report()
+    assert result["average_calls_per_hour"] == 4.0
