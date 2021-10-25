f694b32173195c0b30e9c0b84f043fcfce41de25
Riceyo <richmond@ricey.org>
2021-10-25 11:20:49 +0100

    swapping DialNumber for Id

--- models/appointment_resource_planning_model.py
+++ models/appointment_resource_planning_model.py
@@ -47,7 +47,9 @@ class CatiAppointmentResourcePlanningTable(DataBaseBase):
                   ELSE
                      "English" 
                END
-               AS AppointmentLanguage, dh.DialResult, COUNT(*) AS Total 
+               AS AppointmentLanguage,
+               dh.DialResult,
+               COUNT(*) AS Total 
             FROM
                {cls.table_name()} AS dbci 
                LEFT JOIN
@@ -57,8 +59,7 @@ class CatiAppointmentResourcePlanningTable(DataBaseBase):
                         PrimaryKeyValue,
                         AdditionalData,
                         DialResult,
-                        MAX(StartTime),
-                        DialNumber
+                        Id
                      FROM
                         DialHistory 
                      GROUP BY
@@ -66,16 +67,18 @@ class CatiAppointmentResourcePlanningTable(DataBaseBase):
                         PrimaryKeyValue,
                         AdditionalData,
                         DialResult,
-                        DialNumber
+                        Id
                   )
                   AS dh 
                   ON dh.InstrumentId = dbci.InstrumentId 
                   AND dh.PrimaryKeyValue = dbci.PrimaryKeyValue
-                  AND dh.DialNumber = (
-                                SELECT MAX(DialNumber)
+                  AND dh.DialNumber =
+                                (
+                                SELECT MAX(Id)
                                 FROM cati.DialHistory
                                 WHERE InstrumentId = dbci.InstrumentId
-                                AND PrimaryKeyValue = dbci.PrimaryKeyValue) 
+                                AND PrimaryKeyValue = dbci.PrimaryKeyValue
+                                ) 
             WHERE
                dbci.AppointmentType != "0" 
                AND dbci.AppointmentStartDate like "{date}%" 
