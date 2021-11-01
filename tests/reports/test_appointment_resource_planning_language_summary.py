8e61079207b5b8fa52cfb876050e6ab44c546a07
elthorne <46898565+elthorne@users.noreply.github.com>
2021-11-01 16:30:44 +0000

    tests added for language summary

--- tests/reports/test_appointment_resource_planning_language_summary.py
+++ tests/reports/test_appointment_resource_planning_language_summary.py
@@ -1,25 +1,15 @@
-import os
-from unittest import mock
 from unittest.mock import patch
 
+from models.appointment_resource_planning_language_summary_model import AppointmentResourcePlanningLanguageSummary
 from reports.appointment_resource_planning_language_summary import (
     get_appointment_resource_planning_language_summary_by_date,
 )
 
 
-@mock.patch.dict(
-    os.environ,
-    {
-        "MYSQL_HOST": "foo",
-        "MYSQL_USER": "foo",
-        "MYSQL_PASSWORD": "foo",
-        "MYSQL_DATABASE": "foo",
-    },
-)
 @patch(
     "data_sources.cati_data.get_cati_appointment_resource_planning_from_database_for_language_summary"
 )
-def test_get_appointment_resource_planning_language_summary_by_date_returns_empty_string(
+def test_get_appointment_resource_planning_language_summary_by_date_returns_empty_list(
     mock_get_cati_appointment_resource_planning_from_database_for_language_summary,
 ):
     mock_get_cati_appointment_resource_planning_from_database_for_language_summary.return_value = (
@@ -27,3 +17,79 @@ def test_get_appointment_resource_planning_language_summary_by_date_returns_empt
     )
     result = get_appointment_resource_planning_language_summary_by_date("2021-12-31")
     assert result == []
+
+
+@patch(
+    "data_sources.cati_data.get_cati_appointment_resource_planning_from_database_for_language_summary"
+)
+def test_get_appointment_resource_planning_language_summary_by_date_returns_english_total_only(
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary,
+):
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary.return_value = (
+        [
+            {'AppointmentLanguage': 'English', 'Total': 1}
+        ]
+    )
+    result = get_appointment_resource_planning_language_summary_by_date("2021-12-31")
+    assert result == [AppointmentResourcePlanningLanguageSummary(language='English', total=1)]
+
+
+@patch(
+    "data_sources.cati_data.get_cati_appointment_resource_planning_from_database_for_language_summary"
+)
+def test_get_appointment_resource_planning_language_summary_by_date_returns_english_and_welsh_totals(
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary,
+):
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary.return_value = (
+        [
+            {'AppointmentLanguage': 'English', 'Total': 1},
+            {'AppointmentLanguage': 'Welsh', 'Total': 2}
+        ]
+    )
+    result = get_appointment_resource_planning_language_summary_by_date("2021-12-31")
+    assert result == [AppointmentResourcePlanningLanguageSummary(language='English', total=1),
+                      AppointmentResourcePlanningLanguageSummary(language='Welsh', total=2)]
+
+
+@patch(
+    "data_sources.cati_data.get_cati_appointment_resource_planning_from_database_for_language_summary"
+)
+def test_get_appointment_resource_planning_language_summary_by_date_returns_totals_for_english_welsh_and_other(
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary,
+):
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary.return_value = (
+        [
+            {'AppointmentLanguage': 'English', 'Total': 1},
+            {'AppointmentLanguage': 'Welsh', 'Total': 2},
+            {'AppointmentLanguage': 'Other', 'Total': 3}
+        ]
+    )
+    result = get_appointment_resource_planning_language_summary_by_date("2021-12-31")
+    assert result == [AppointmentResourcePlanningLanguageSummary(language='English', total=1),
+                      AppointmentResourcePlanningLanguageSummary(language='Welsh', total=2),
+                      AppointmentResourcePlanningLanguageSummary(language='Other', total=3)
+                      ]
+
+
+@patch(
+    "data_sources.cati_data.get_cati_appointment_resource_planning_from_database_for_language_summary"
+)
+def test_get_appointment_resource_planning_language_summary_by_date_returns_totals_for_english_welsh_other_and_new_languages(
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary,
+):
+    mock_get_cati_appointment_resource_planning_from_database_for_language_summary.return_value = (
+        [
+            {'AppointmentLanguage': 'English', 'Total': 1},
+            {'AppointmentLanguage': 'Welsh', 'Total': 2},
+            {'AppointmentLanguage': 'Other', 'Total': 3},
+            {'AppointmentLanguage': 'Sarcasm', 'Total': 4},
+            {'AppointmentLanguage': 'Profanity', 'Total': 5}
+        ]
+    )
+    result = get_appointment_resource_planning_language_summary_by_date("2021-12-31")
+    assert result == [AppointmentResourcePlanningLanguageSummary(language='English', total=1),
+                      AppointmentResourcePlanningLanguageSummary(language='Welsh', total=2),
+                      AppointmentResourcePlanningLanguageSummary(language='Other', total=3),
+                      AppointmentResourcePlanningLanguageSummary(language='Sarcasm', total=4),
+                      AppointmentResourcePlanningLanguageSummary(language='Profanity', total=5)
+                      ]
