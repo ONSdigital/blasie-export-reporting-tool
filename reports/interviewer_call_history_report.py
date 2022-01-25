eee5f9a157080729b119d716b75bdb350771795d
elthorne <46898565+elthorne@users.noreply.github.com>
2022-01-25 16:17:54 +0000

    Refactor: Call History Report
    Broke down get_call_history_records() in to smaller functions to improve readability
    
    Reviewed-by: El
    Refs: BLAIS5-2988

--- reports/interviewer_call_history_report.py
+++ reports/interviewer_call_history_report.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from google.cloud import datastore
 
 from functions.date_functions import parse_date_string_to_datetime
@@ -7,12 +5,27 @@ from models.error_capture import BertException
 
 
 def get_call_history_records(interviewer_name, start_date_string, end_date_string, survey_tla=None):
+    start_date, end_date = parse_dates(start_date_string, end_date_string)
+    if is_invalid(start_date) or is_invalid(end_date):
+        raise BertException("Invalid date range parameters provided", 400)
+    records = get_datastore_records(interviewer_name, start_date, end_date, survey_tla)
+    results = identify_webnudge_cases(records)
+    return results
+
+
+def parse_dates(start_date_string, end_date_string):
     start_date = parse_date_string_to_datetime(start_date_string)
     end_date = parse_date_string_to_datetime(end_date_string, True)
-    print(f"Getting call history data for interviewer '{interviewer_name}' between '{start_date}' and '{end_date}'")
-    if start_date is None or end_date is None:
-        raise BertException("Invalid date range parameters provided", 400)
+    return start_date, end_date
 
+
+def is_invalid(date):
+    if date is None:
+        return True
+
+
+def get_datastore_records(interviewer_name, start_date, end_date, survey_tla):
+    print(f"Getting call history data for interviewer '{interviewer_name}' between '{start_date}' and '{end_date}'")
     client = datastore.Client()
     query = client.query(kind="CallHistory")
     query.add_filter("interviewer", "=", interviewer_name)
@@ -24,11 +37,14 @@ def get_call_history_records(interviewer_name, start_date_string, end_date_strin
         query.add_filter("survey", "=", survey_tla)
 
     query.order = ["call_start_time"]
-    results = list(query.fetch())
+    records = list(query.fetch())
+    print(f"get_call_history_records_by_interviewer_and_date_range - {len(records)} records found")
+    return records
 
-    for record in results:
+
+def identify_webnudge_cases(records):
+    for record in records:
         if record['outcome_code'] == "120":
             record["call_result"] = "WebNudge"
 
-    print(f"get_call_history_records_by_interviewer_and_date_range - {len(results)} records found")
-    return results
+    return records
