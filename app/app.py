347a516ea7ce63b650e7f33ed8d11fe2add1b763
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-04-13 15:04:01 +0100

    feature: update bert endpoint to include questionniares
    Reviewed-by: James & Tom
    Refs: BLAIS5-3036

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from google.cloud import datastore
 from data_sources.call_history_data import CallHistoryClient
 from models.config_model import Config
 from models.error_capture import BertException
-from functions.request_handlers import date_handler, survey_tla_handler
+from functions.request_handlers import date_handler, survey_tla_handler, questionnaire_handler
 from reports.appointment_resource_planning_report import get_appointment_resource_planning_by_date
 from reports.appointment_language_summary import get_appointment_language_summary_by_date
 from reports.interviewer_call_history_report import get_call_history_records, get_call_history_questionnaires
@@ -31,7 +31,8 @@ def call_history_report_status():
 def call_history(interviewer):
     start_date, end_date = date_handler(request)
     survey_tla = survey_tla_handler(request)
-    return jsonify(get_call_history_records(interviewer, start_date, end_date, survey_tla))
+    questionnaires = questionnaire_handler(request)
+    return jsonify(get_call_history_records(interviewer, start_date, end_date, survey_tla, questionnaires))
 
 
 @app.route("/api/reports/call-history/<interviewer>/questionnaires")
