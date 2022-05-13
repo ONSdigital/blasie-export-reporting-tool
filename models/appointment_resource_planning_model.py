861531c3831f5fb059561732c3d8056d806375e4
calnic <98323072+cal-nic@users.noreply.github.com>
2022-05-13 15:36:37 +0100

    fix sql query so that OR statement works

--- models/appointment_resource_planning_model.py
+++ models/appointment_resource_planning_model.py
@@ -40,8 +40,8 @@ class CatiAppointmentResourcePlanningTable(DataBaseBase):
                 INNER JOIN
                     configuration.Configuration cf
                 ON dh.InstrumentId = cf.InstrumentId
-                WHERE (NULLIF({questionnaire_filter}, '') IS NOT NULL AND cf.InstrumentName IN ({questionnaire_filter})) 
-                    OR (cf.InstrumentName LIKE '{str(survey_tla or '')}%')
+                WHERE (LENGTH({questionnaire_filter}) > 0 AND cf.InstrumentName IN ({questionnaire_filter}))
+                    OR (LENGTH({questionnaire_filter}) = 0 AND cf.InstrumentName LIKE '{str(survey_tla or '')}%')
                 GROUP BY
                     dh.PrimaryKeyValue,
                     dh.InstrumentId)
