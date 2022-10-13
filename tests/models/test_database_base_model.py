d56065b0a2bf1f0b4fc8b18aa72b4b7247963152
elthorne <46898565+elthorne@users.noreply.github.com>
2022-10-13 13:36:50 +0100

    Refactor: REfactored Database Base Model and tested.
    Reviewed-by: El and Tom
    Refs: BLAIS5-3101

---
+++ tests/models/test_database_base_model.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from unittest.mock import patch
+
+from models.database_base_model import DataBaseBase
+
+
+@dataclass
+class ExampleTableModel(DataBaseBase):
+    field_name_1: str
+    field_name_2: int
+    field_name_3: bool
+
+    @classmethod
+    def table_name(cls):
+        return "foo.bar"
+
+
+@patch("models.database_base_model.DataBaseBase.query")
+def test_database_base_model_select_from_calls_query_with_the_correct_parameters(query, config):
+    # arrange & act
+    ExampleTableModel.select_from(config)
+
+    # assert
+    query.assert_called_with(config, "SELECT field_name_1, field_name_2, field_name_3 FROM foo.bar")
+
+
+@patch("models.database_base_model.DataBaseBase.query")
+def test_database_base_model_select_from_returns_the_expected_query_result(query, config):
+    # arrange
+    query.return_value = [
+        ("value_1_for_field_name_1", 1, False),
+        ("value_2_for_field_name_1", 2, True),
+    ]
+
+    # act
+    result = ExampleTableModel.select_from(config)
+
+    # assert
+    assert result == [
+        ("value_1_for_field_name_1", 1, False),
+        ("value_2_for_field_name_1", 2, True),
+    ]
