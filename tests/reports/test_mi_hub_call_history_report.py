5e8952982ced0912b3d5a1d667c12099566cb12b
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-03-03 15:22:41 +0000

    feat: remove unwanted single quotes from cohort field refs: BLAIS5-3633

--- tests/reports/test_mi_hub_call_history_report.py
+++ tests/reports/test_mi_hub_call_history_report.py
@@ -7,24 +7,6 @@ from reports.mi_hub_call_history_report import get_mi_hub_call_history
 from unittest import mock
 
 
-@pytest.fixture
-def mock_get_mi_hub_call_history_record():
-    return {
-        "InstrumentId": "1234",
-        "PrimaryKeyValue": "9000001",
-        "CallNumber": 700000,
-        "DialNumber": 700000,
-        "Interviewer": "James",
-        "DialResult": 300,
-        "DialedNumber": 700000,
-        "dial_secs": 500,
-        "OutcomeCode": "310",
-        "Cohort": "'AA'",
-        "StartTime": datetime(2023, 2, 3, 20, 5, 0),
-        "EndTime": datetime(2023, 2, 3, 20, 10, 0),
-    }
-
-
 @mock.patch("reports.mi_hub_call_history_report.get_cati_mi_hub_call_history_from_database")
 def test_mi_hub_call_history_returns_something(mock_mi_hub_call_history):
     mock_mi_hub_call_history.return_value = [
@@ -62,13 +44,3 @@ def test_mi_hub_call_history_returns_something(mock_mi_hub_call_history):
             end_time="20:10:00",
         )
     ]
-
-
-@mock.patch("reports.mi_hub_call_history_report.get_cati_mi_hub_call_history_from_database")
-def test_mi_hub_call_history_returns_cohort_field_without_quotes_when_quotes_are_present(mock_mi_hub_call_history, mock_get_mi_hub_call_history_record):
-    mock_get_mi_hub_call_history_record["Cohort"]="'AA'"
-    mock_mi_hub_call_history.return_value = [
-        mock_get_mi_hub_call_history_record
-    ]
-    result = get_mi_hub_call_history("foo", "LMS2101_AA1", "1234")
-    assert result[0].cohort == "AA"
