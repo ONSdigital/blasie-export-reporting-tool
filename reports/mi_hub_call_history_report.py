5cf337b9e8968210b5ced6824b12a16d1f78f13f
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-03-08 14:44:52 +0000

    refactor: refactor unit tests refs: BLAIS5-3633

--- reports/mi_hub_call_history_report.py
+++ reports/mi_hub_call_history_report.py
@@ -5,24 +5,29 @@ from models.mi_hub_call_history_model import MiHubCallHistory
 def get_mi_hub_call_history(config, questionnaire_name, questionnaire_id):
     print(f"Getting MI hub call history report data for {questionnaire_name}")
     cati_data = get_cati_mi_hub_call_history_from_database(config)
-    cati_mi_hub_call_history_list = []
-    for item in cati_data:
-        if item.get("InstrumentId") == questionnaire_id:
-            cati_mi_hub_call_history = MiHubCallHistory(
-                questionnaire_id=item.get("InstrumentId"),
-                serial_number=item.get("PrimaryKeyValue"),
-                call_number=item.get("CallNumber"),
-                dial_number=item.get("DialNumber"),
-                interviewer=item.get("Interviewer"),
-                dial_result=item.get("DialResult"),
-                dial_line_number=item.get("DialedNumber"),
-                seconds_interview=item.get("dial_secs"),
-                outcome_code=item.get("OutcomeCode"),
-                cohort=item.get("Cohort").replace("'", ""),
-            )
-            cati_mi_hub_call_history.generate_dial_date_and_time_fields(
-                item.get("StartTime"), item.get("EndTime")
-            )
-            cati_mi_hub_call_history.questionnaire_name = questionnaire_name
-            cati_mi_hub_call_history_list.append(cati_mi_hub_call_history)
-    return cati_mi_hub_call_history_list
+
+    return [
+        create_mi_hub_call_history(item, questionnaire_name)
+        for item in cati_data
+        if item.get("InstrumentId") == questionnaire_id
+    ]
+
+
+def create_mi_hub_call_history(item, questionnaire_name):
+    cati_mi_hub_call_history = MiHubCallHistory(
+        questionnaire_id=item.get("InstrumentId"),
+        serial_number=item.get("PrimaryKeyValue"),
+        call_number=item.get("CallNumber"),
+        dial_number=item.get("DialNumber"),
+        interviewer=item.get("Interviewer"),
+        dial_result=item.get("DialResult"),
+        dial_line_number=item.get("DialedNumber"),
+        seconds_interview=item.get("dial_secs"),
+        outcome_code=item.get("OutcomeCode"),
+        cohort=item.get("Cohort").replace("'", ""),
+    )
+    cati_mi_hub_call_history.generate_dial_date_and_time_fields(
+        item.get("StartTime"), item.get("EndTime")
+    )
+    cati_mi_hub_call_history.questionnaire_name = questionnaire_name
+    return cati_mi_hub_call_history
