9c3bae3ac0f28f9fbbf1f8c7d84268ed70ed3aae
elthorne <46898565+elthorne@users.noreply.github.com>
2023-06-06 15:36:08 +0100

    refactor: Only One Level of Indentation per Method
    This commit is the basic extraction of a level of indentation to a function

--- reports/mi_hub_call_history_report.py
+++ reports/mi_hub_call_history_report.py
@@ -2,30 +2,42 @@ from data_sources.cati_data import get_cati_mi_hub_call_history_from_database
 from models.mi_hub_call_history_model import MiHubCallHistory
 
 
+def populate_call_history_model(item, questionnaire_id, questionnaire_name, cati_mi_hub_call_history_list):
+    if item.get("InstrumentId") == questionnaire_id:
+        cati_mi_hub_call_history = MiHubCallHistory(
+            questionnaire_name=questionnaire_name,
+            questionnaire_id=item.get("InstrumentId"),
+            serial_number=item.get("PrimaryKeyValue"),
+            call_number=item.get("CallNumber"),
+            dial_number=item.get("DialNumber"),
+            interviewer=item.get("Interviewer"),
+            dial_result=item.get("DialResult"),
+            dial_line_number=item.get("DialedNumber"),
+            seconds_interview=item.get("dial_secs"),
+            outcome_code=item.get("OutcomeCode"),
+            cohort=get_cohort(item),
+        )
+        cati_mi_hub_call_history.generate_dial_date_and_time_fields(
+            item.get("StartTime"), item.get("EndTime")
+        )
+        cati_mi_hub_call_history_list.append(cati_mi_hub_call_history)
+
+        return cati_mi_hub_call_history_list
+
+
 def get_mi_hub_call_history(config, questionnaire_name, questionnaire_id):
     print(f"Getting MI hub call history report data for {questionnaire_name}")
     cati_data = get_cati_mi_hub_call_history_from_database(config)
     cati_mi_hub_call_history_list = []
 
     for item in cati_data:
-        if item.get("InstrumentId") == questionnaire_id:
-            cati_mi_hub_call_history = MiHubCallHistory(
-                questionnaire_name=questionnaire_name,
-                questionnaire_id=item.get("InstrumentId"),
-                serial_number=item.get("PrimaryKeyValue"),
-                call_number=item.get("CallNumber"),
-                dial_number=item.get("DialNumber"),
-                interviewer=item.get("Interviewer"),
-                dial_result=item.get("DialResult"),
-                dial_line_number=item.get("DialedNumber"),
-                seconds_interview=item.get("dial_secs"),
-                outcome_code=item.get("OutcomeCode"),
-                cohort=get_cohort(item),
-            )
-            cati_mi_hub_call_history.generate_dial_date_and_time_fields(
-                item.get("StartTime"), item.get("EndTime")
-            )
-            cati_mi_hub_call_history_list.append(cati_mi_hub_call_history)
+        cati_mi_hub_call_history_list = populate_call_history_model(
+            item,
+            questionnaire_id,
+            questionnaire_name,
+            cati_mi_hub_call_history_list
+        )
+
     return cati_mi_hub_call_history_list
 
 
