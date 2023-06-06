b174b782d48f25a1537cf84d5efb9d7282c7d626
elthorne <46898565+elthorne@users.noreply.github.com>
2023-06-06 15:49:35 +0100

    refactor: Only One Level of Indentation per Method
    renaming of variables

--- reports/mi_hub_call_history_report.py
+++ reports/mi_hub_call_history_report.py
@@ -2,7 +2,7 @@ from data_sources.cati_data import get_cati_mi_hub_call_history_from_database
 from models.mi_hub_call_history_model import MiHubCallHistory
 
 
-def populate_call_history_model(item, questionnaire_id, questionnaire_name, cati_mi_hub_call_history_list):
+def populate_call_history_model(item, questionnaire_id, questionnaire_name, call_history_data):
     if item.get("InstrumentId") == questionnaire_id:
         cati_mi_hub_call_history = MiHubCallHistory(
             questionnaire_name=questionnaire_name,
@@ -20,25 +20,25 @@ def populate_call_history_model(item, questionnaire_id, questionnaire_name, cati
         cati_mi_hub_call_history.generate_dial_date_and_time_fields(
             item.get("StartTime"), item.get("EndTime")
         )
-        cati_mi_hub_call_history_list.append(cati_mi_hub_call_history)
+        call_history_data.append(cati_mi_hub_call_history)
 
-        return cati_mi_hub_call_history_list
+        return call_history_data
 
 
 def get_mi_hub_call_history(config, questionnaire_name, questionnaire_id):
     print(f"Getting MI hub call history report data for {questionnaire_name}")
     cati_data = get_cati_mi_hub_call_history_from_database(config)
-    cati_mi_hub_call_history_list = []
+    call_history_data = []
 
     for item in cati_data:
-        cati_mi_hub_call_history_list = populate_call_history_model(
+        call_history_data = populate_call_history_model(
             item,
             questionnaire_id,
             questionnaire_name,
-            cati_mi_hub_call_history_list
+            call_history_data
         )
 
-    return cati_mi_hub_call_history_list
+    return call_history_data
 
 
 def get_cohort(item):
