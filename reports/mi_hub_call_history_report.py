342776cf1c336f5b85b754316101ceedc271f377
elthorne <46898565+elthorne@users.noreply.github.com>
2023-06-06 17:38:09 +0100

    refactor: inline

--- reports/mi_hub_call_history_report.py
+++ reports/mi_hub_call_history_report.py
@@ -4,8 +4,8 @@ from models.mi_hub_call_history_model import CallHistoryReport
 
 def get_mi_hub_call_history(config, questionnaire_name, questionnaire_id):
     print(f"Getting MI hub call history report data for {questionnaire_name}")
-    cati_data = get_cati_mi_hub_call_history_from_database(config)
-    call_history_report = CallHistoryReport(cati_data)
+    call_history_report = CallHistoryReport(
+        get_cati_mi_hub_call_history_from_database(config))
 
     call_history_report.populate(questionnaire_id, questionnaire_name)
 
