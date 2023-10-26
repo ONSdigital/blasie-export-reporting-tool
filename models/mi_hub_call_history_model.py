fadb2fca58c1ed4c7fa183d91e0201a16b85e3cd
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-10-26 09:34:37 +0100

    refactor: remove pytz

--- models/mi_hub_call_history_model.py
+++ models/mi_hub_call_history_model.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass, fields
 from datetime import datetime, timedelta
 from typing import Optional, Union
 
-import pytz
-
 from models.database_base_model import DatabaseBase
 
 
