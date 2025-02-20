594727d1f53111ce2f8716d2a6f2b4623e182eb8
lambeb <141648830+lambeb@users.noreply.github.com>
2025-02-20 11:47:27 +0000

    Added fields to class and get_mi_hub_respondent_data() function.

--- models/mi_hub_respondent_data_model.py
+++ models/mi_hub_respondent_data_model.py
@@ -12,6 +12,51 @@ class MiHubRespondentData:
     gender: str
     date_of_birth: str
     age: str
+    Case_ID: str
+    ShiftNo: str
+    Interv: str
+    CaseNotes: str
+    Flight1: str
+    Flight2: str
+    Flight3: str
+    Flight4: str
+    Flight5: str
+    Flight6: str
+    Flight7: str
+    Flight8: str
+    IntDate: str
+    SelectionTime: str
+    DMExitTime: str
+    IntType: str
+    SampInterval: str
+    ShiftType: str
+    Portroute: str
+    Baseport: str
+    Linecode: str
+    FlightNum: str
+    DVFlightNum: str
+    PortCode: str
+    PortDestination: str
+    Shuttle: str
+    CrossShut: str
+    Vehicle: str
+    IsElig: str
+    FerryTime: str
+    Flow: str
+    DVRespnse: str
+    proportion: str
+    response_visitbritain: str
+    response_age_sex: str
+    response_student: str
+    response_fe_trailer: str
+    response_migration_trailer: str
+    DMTimeIsElig: str
+    DMTimeAgeSex: str
+    UKForeign: str
+    StudyCheck: str
+    Expenditure: str
+    Age: str
+    Sex: str
 
     @classmethod
     def fields(cls):
